8f361de6183a66ffaab45e98bfa2eb42ba48a714
Pooja Parthasarathy <ppartha0@chicagobooth.edu>
2017-07-10 00:16:56 +0000

    Added latitude and longitude queries to function

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -9,17 +9,17 @@ class GeocodingController < ApplicationController
   def street_to_coords
     @street_address = params[:user_street_address]
 
-    # ==========================================================================
-    # Your code goes below.
-    #
-    # The street address that the user typed is in the variable @street_address.
-    # ==========================================================================
-
-
-
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+   ### Substitute any blank spaces with +s
+   scrubbed_address = @street_address.gsub(" ","+")
+   ### Now put that into the Google API
+   default_api = "https://maps.googleapis.com/maps/api/geocode/json?address="
+   address_api = default_api + scrubbed_address
+   ### Query the resulting API to retrieve "geometry[location][lat] and [long]"  
+   raw_data = open(address_api).read
+   parsed_data = JSON.parse(raw_data)
+   location_data = parsed_data["results"][0]["geometry"]["location"]
+    @latitude = location_data["lat"]
+    @longitude = location_data["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
